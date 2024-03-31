94cb3585328874dc65e6115b770437c990f08737
Jonathan <carusyte@163.com>
2024-03-31 15:48:16 +0800

    update shell

--- src/models/run.sh
+++ src/models/run.sh
@@ -4,4 +4,4 @@ script=gridsearch.py
 PYENV_PYTHON=$(pyenv which python)
 >grid_search.log
 # nohup "$PYENV_PYTHON" "$script" "--worker=14" "--top_n=100" "--epochs=500" "930955" > /dev/null 2>&1 &
-nohup "$PYENV_PYTHON" "$script" "$@" > /dev/null 2>&1 &
+nohup "$PYENV_PYTHON" "$script" "$@" > >(tee -a output.log) 2> >(tee -a error.log >&2) &
