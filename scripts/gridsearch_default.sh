b485f3f9a1095dfa621d1655e6ed51232ba13c72
Jonathan <carusyte@163.com>
2024-04-09 00:16:05 +0800

    update shell

--- scripts/gridsearch_default.sh
+++ scripts/gridsearch_default.sh
@@ -6,6 +6,7 @@ OUTPUT_LOG=output_gs.log
 
 >grid_search.log
 >$OUTPUT_LOG
+>marten.models.gridsearch.log
 
 # nohup "$PYENV_PYTHON" "$script" "--worker=14" "--top_n=100" "--epochs=500" "930955" > /dev/null 2>&1 &
 nohup "$CLI" "gs" "$@" > >(tee -a $OUTPUT_LOG) 2>&1 &
