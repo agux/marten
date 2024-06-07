4e7dea37647e90c614c4a4bd6edaeae93635c884
Jonathan <carusyte@163.com>
2024-06-07 18:47:09 +0800

    fix etl

--- src/marten/cli/commands/etl.py
+++ src/marten/cli/commands/etl.py
@@ -18,7 +18,7 @@ def configure_parser(parser):
         "--threads",
         action="store",
         type=int,
-        default=4,
+        default=3,
         help=(
             "Number of threads per worker (python processes) for data retrieval. "
             "Defaults to 3."
