c863486b19507063e254aaaf9023cfe490602992
Jonathan <carusyte@163.com>
2024-11-14 11:51:35 +0800

    fix

--- src/marten/models/base_model.py
+++ src/marten/models/base_model.py
@@ -161,7 +161,7 @@ class BaseModel(ABC):
             if lock_acquired is None and self._check_cpu():
                 lock = Lock(cpu_lock_key)
                 gpu_tried = max(0, gpu_tried - 0.8)
-                if lock.acquire(timeout=f"{self.lock_wait_time}s"):
+                if lock.acquire(timeout=f"{self.lock_wait_time}"):
                     lock_acquired = lock
                     get_logger().debug("lock acquired: %s", cpu_lock_key)
 
