5674032320202869ab4120781f1ca0b11a81cbe9
carusyte <carusyte@163.com>
2024-11-24 10:47:31 +0800

    fix

--- src/marten/utils/logger.py
+++ src/marten/utils/logger.py
@@ -29,18 +29,19 @@ class WorkerMessageFilter(logging.Filter):
 
 
 def get_logger(name=None, role: Literal["client", "worker"] = "client") -> Logger:
-    global logger
+    # global logger
 
-    if logger is not None:
-        return logger
-
-    load_dotenv()
-
-    level = os.getenv("LOG_LEVEL")
-    level = level.upper() if level else None
+    # if logger is not None:
+    #     return logger
 
     logger = logging.getLogger(name if name is not None else __name__)
+
     if not logger.handlers:  # Check if the logger already has handlers
+        load_dotenv()
+
+        level = os.getenv("LOG_LEVEL")
+        level = level.upper() if level else None
+        
         logger.setLevel(logging.INFO if level is None else level)
 
         formatter = None
