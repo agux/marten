51227bc8b4d07567d1297a57faa9884376387e80
carusyte <carusyte@163.com>
2024-12-14 23:45:40 +0800

    backup db

--- scripts/backup_db.sh
+++ scripts/backup_db.sh
@@ -8,12 +8,18 @@ else
   exit 1
 fi
 
-
 SOURCE="postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}"
 # Get the number of CPU cores
 # NUM_CORES=$(nproc)
 # Calculate 90% of the number of CPU cores
 # DEFAULT_NWORKERS=$(echo "$NUM_CORES * 0.9 / 1" | bc)
+
+export PGPASSWORD="$DB_PASSWORD"
+psql -U "$DB_USER" -h "$DB_HOST" -p $DB_PORT -d "$DB_NAME" \
+  -c "VACUUM;" \
+  -c "REINDEX DATABASE $DB_NAME;" \
+  -c "ANALYZE;"
+unset PGPASSWORD
 
 # get a .sql backup file name with timestamp as suffix
 SCHEMA_FILE="schema_$(date +"%Y%m%d%H%M%S").sql"
