c7bc78f89941566b0553c96c6e2e0eacad44b519
carusyte <carusyte@163.com>
2025-01-07 20:44:40 +0800

    fix

--- src/marten/data/ta.py
+++ src/marten/data/ta.py
@@ -229,7 +229,7 @@ def save_ta(ta_table, df, symbol):
         )
         last_date = result.fetchone()[0]
     df.loc[:, "date"] = pd.to_datetime(df["date"]).dt.date
-    worker.info(
+    worker.logger.info(
         "%s@%s, last_date:%s, before: %s, sample date: %s",
         symbol,
         ta_table.__tablename__,
@@ -239,7 +239,7 @@ def save_ta(ta_table, df, symbol):
     )
     if last_date is not None:
         df = df[df["date"] > (last_date - timedelta(days=1))]
-    worker.info(
+    worker.logger.info(
         "%s@%s, last_date:%s, after: %s, sample date: %s",
         symbol,
         ta_table.__tablename__,
