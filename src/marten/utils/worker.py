f200dee4b6571bc19eaac4cb0170d42dd512dbde
carusyte <carusyte@163.com>
2025-01-08 19:36:20 +0800

    print environment variable

--- src/marten/utils/worker.py
+++ src/marten/utils/worker.py
@@ -96,13 +96,15 @@ class LocalWorkerPlugin(WorkerPlugin):
                 "OMP_NUM_THREADS: %s\n"
                 "MKL_NUM_THREADS: %s\n"
                 "OPENBLAS_NUM_THREADS: %s\n"
-                "NUMEXPR_NUM_THREADS: %s"
+                "NUMEXPR_NUM_THREADS: %s\n"
+                "VECLIB_MAXIMUM_THREADS: %s\n"
             ),
             os.getenv("MALLOC_TRIM_THRESHOLD_"),
             os.getenv("OMP_NUM_THREADS"),
             os.getenv("MKL_NUM_THREADS"),
             os.getenv("OPENBLAS_NUM_THREADS"),
             os.getenv("NUMEXPR_NUM_THREADS"),
+            os.getenv("VECLIB_MAXIMUM_THREADS"),
         )
 
 
