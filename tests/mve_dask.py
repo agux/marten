92c63bd25dd8f7a603f6a478a052a6e557ae6e00
Jonathan <carusyte@163.com>
2025-01-27 17:59:56 +0800

    tweak mve

--- tests/mve_dask.py
+++ tests/mve_dask.py
@@ -7,7 +7,7 @@ total_workload = 2e4
 n_workers = 16
 
 
-def dummy_task(i):
+def gil_holding_task(i):
     print(f"working on task #{i}")
     duration = random.randint(5, 10)
     end_time = time.perf_counter() + duration
@@ -19,7 +19,7 @@ def dummy_task(i):
 
 def main():
     cluster = LocalCluster(
-        n_workers=n_workers,
+        n_workers=1,
         threads_per_worker=1,
         processes=True,
         silence_logs=logging.INFO,
@@ -28,10 +28,14 @@ def main():
     client = Client(cluster)
 
     futures = []
+    upscaled = False
 
     for i in range(int(total_workload)):
-        futures.append(client.submit(dummy_task, i))
-        if len(futures) > n_workers * 3:
+        futures.append(client.submit(gil_holding_task, i))
+        if len(futures) > 1:
+            if not upscaled:
+                cluster.scale(n_workers)
+                upscaled = True
             _, undone = wait(futures, return_when="FIRST_COMPLETED")
             futures = list(undone)
 
