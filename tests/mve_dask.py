55590099036f8a3d349565d1348efa1ee9af3666
carusyte <carusyte@163.com>
2025-01-27 21:26:08 +0800

    tweak mve

--- tests/mve_dask.py
+++ tests/mve_dask.py
@@ -1,30 +1,47 @@
 import faulthandler
+
 faulthandler.enable()
 
 import random
 import logging
 import time
 from datetime import datetime
-from dask.distributed import Client, LocalCluster, wait
+from dask.distributed import Client, LocalCluster, worker_client, get_worker, wait
 
-total_workload = 5e4
+total_workload = 2e5
 n_workers = 16
 
 logger = logging.getLogger(__name__)
 
-def gil_holding_task(i):
-    logger.error(f"{datetime.now().strftime("%Y-%m-%d %H:%M:%S")} working on task #{i}")
+
+def tier2_task(i):
+    logger.error(
+        f'{datetime.now().strftime("%Y-%m-%d %H:%M:%S")} worker#{get_worker().name} on tier2 task #{i}'
+    )
     duration = random.randint(5, 10)
     end_time = time.perf_counter() + duration
     result = 0
     while time.perf_counter() < end_time:
-        result += 1
+        result += random.uniform(-1, 1)
     return result
+
+
+def tier1_task():
+    futures = []
+    with worker_client() as client:
+        for i in range(int(total_workload)):
+            futures.append(client.submit(tier2_task, i))
+            if len(futures) > n_workers:
+                _, undone = wait(futures, return_when="FIRST_COMPLETED")
+                futures = list(undone)
+    logger.error(
+        f'{datetime.now().strftime("%Y-%m-%d %H:%M:%S")} worker#{get_worker().name} waiting ALL_COMPLETED'
+    )
+    wait(futures)
 
 
 def main():
     cluster = LocalCluster(
-        # n_workers=1,
         n_workers=n_workers,
         threads_per_worker=1,
         processes=True,
@@ -34,19 +51,15 @@ def main():
     client = Client(cluster)
 
     futures = []
-    # upscaled = False
 
-    for i in range(int(total_workload)):
-        futures.append(client.submit(gil_holding_task, i))
-        if len(futures) > n_workers:
-        # if len(futures) > 1:
-            # if not upscaled:
-            #     cluster.scale(n_workers)
-            #     upscaled = True
+    for _ in range(10):
+        futures.append(client.submit(tier1_task))
+        if len(futures) > 1:
             _, undone = wait(futures, return_when="FIRST_COMPLETED")
             futures = list(undone)
-    
+
     wait(futures)
+    logger.info("all tasks completed.")
 
 
 if __name__ == "__main__":
