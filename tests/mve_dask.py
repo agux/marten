9d40c6f9c745acbbb3562f7ab6fecd5ef063acef
carusyte <carusyte@163.com>
2025-02-01 22:26:01 +0800

    mve

--- tests/mve_dask.py
+++ tests/mve_dask.py
@@ -1,9 +1,4 @@
-import faulthandler
-
-faulthandler.enable()
-
 import os
-
 os.environ["PYTHONUNBUFFERED"] = "1"
 
 import uuid
@@ -31,11 +26,6 @@ from marten.utils.worker import init_client
 n_workers = 64
 num_tier1_tasks = 10
 num_tier2_tasks = 2e4
-
-logger = logging.getLogger(__name__)
-logger.setLevel(logging.INFO)
-cluster = None
-client = None
 
 
 class LocalWorkerPlugin(WorkerPlugin):
@@ -66,7 +56,7 @@ def make_df():
     return pd.DataFrame({"Date": date_range, "val1": val1, "val2": val2})
 
 
-def tier1_task(i1, p, num_tier2_tasks, locks, df):
+def tier1_task(i1, p, num_tier2_tasks, locks):
     futures = []
     start_time = datetime.now()
     i2 = 0
@@ -78,7 +68,6 @@ def tier1_task(i1, p, num_tier2_tasks, locks, df):
                     i1,
                     i2,
                     locks,
-                    df,
                     priority=p,
                     key=f"tier2_task_{i1}-{uuid.uuid4().hex}",
                 )
@@ -94,72 +83,29 @@ def tier1_task(i1, p, num_tier2_tasks, locks, df):
         wait(futures)
 
 
-def tier2_task(i1, i2, locks, df):
+def tier2_task(i1, i2, locks):
     print(
         f'{datetime.now().strftime("%Y-%m-%d %H:%M:%S")} worker#{get_worker().name} on tier2 task #{i1}:{i2}'
     )
 
-    duration = random.uniform(5, 10)
+    duration = random.uniform(3, 8)
     time.sleep(duration)
 
     return None
 
 
 def main():
-    global client, cluster
-
-    # logger.error(
-    #     "distributed.admin.event-loop: %s",
-    #     dask.config.get("distributed.admin.event-loop"),
-    # )
-
-    # dask.config.set(
-    #     {
-    #         "distributed.worker.memory.terminate": False,
-    #         "distributed.worker.resources.POWER": 2,
-    #         "distributed.worker.multiprocessing-method": "spawn",
-    #         "distributed.deploy.lost-worker-timeout": "2 hours",
-    #         "distributed.scheduler.locks.lease-timeout": "15 minutes",
-    #         "distributed.comm.retry.count": 10,
-    #         "distributed.admin.log-length": 0,
-    #         "distributed.admin.low-level-log-length": 0,
-    #         "distributed.admin.system-monitor.log-length": 0,
-    #     }
-    # )
-    # cluster = LocalCluster(
-    #     n_workers=n_workers,
-    #     threads_per_worker=1,
-    #     scheduler_port=1234,
-    #     dashboard_address=":8787",
-    #     processes=True,
-    #     silence_logs=logging.INFO,
-    #     memory_limit=0,
-    # )
-    # client = Client(cluster, direct_to_workers=True, connection_limit=4096)
-    # client.register_plugin(LocalWorkerPlugin())
-    # client.forward_logging()
-    client = init_client(
-        "mve",
-        max_worker=n_workers,
-        args=SimpleNamespace(
-            model="tsmixerx",
-            dask_log=True,
-            scheduler_port=8999,
-            min_worker=4,
-            max_worker=32,
-        ),
+    cluster = LocalCluster(
+        n_workers=n_workers,
+        threads_per_worker=1,
+        processes=True,
     )
+    client = Client(cluster)
 
-    # dask.config.set({"distributed.scheduler.locks.lease-timeout": "500s"})
-    dask.config.set({"distributed.scheduler.locks.lease-timeout": "20s"})
     locks = {
-        "lock1": Semaphore(max_leases=2, name="dummy_semaphore1"),
+        "lock1": Semaphore(max_leases=1, name="dummy_semaphore1"),
         "lock2": Semaphore(max_leases=1, name="dummy_semaphore2"),
     }
-
-    df = make_df()
-
-    client.submit(tier2_task, 0, 0, locks, df).result()
 
     futures = []
     for i1 in range(num_tier1_tasks):
@@ -171,9 +117,6 @@ def main():
                 p,
                 num_tier2_tasks,
                 locks,
-                df,
-                priority=p,
-                key=f"tier1_task_{i1}-{uuid.uuid4().hex}",
             )
         )
         if len(futures) > 1:
